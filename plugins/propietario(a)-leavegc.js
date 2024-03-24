c6fa5adc3987fa371aba37024647135f1a6a8067
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-03-24 06:02:55 +0100

    𝗣𝗶𝗻𝗴𝘂𝗶 𝗗𝗶𝗼𝘀 𝗦𝗲𝘅 🐧

--- plugins/propietario(a)-leavegc.js
+++ plugins/propietario(a)-leavegc.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let chat = global.db.data.chats[m.chat]
 chat.welcome = false
-await conn.reply(id, `*${wm} 𝘼𝘽𝘼𝙉𝘿𝙊𝙉𝘼 𝙀𝙇 𝙂𝙍𝙐𝙋𝙊, 𝙁𝙐𝙀 𝙂𝙀𝙉𝙄𝘼𝙇 𝙀𝙎𝙏𝘼𝙍 𝘼𝙌𝙐𝙄 👋*`) 
+await conn.reply(id, `${wm} 𝗚𝗿𝗮𝗰𝗶𝗮𝘀 , 𝗠𝗲 𝗥𝗲𝘁𝗶𝗿𝗼 𝗣𝗮𝗿𝗮 𝗩𝗼𝗹𝘃𝗲𝗿 𝗘𝘀𝗰𝗿𝗶𝗯𝗲𝗺𝗲 𝗔𝗹 +𝟱𝟭 𝟵𝟮𝟲 𝟵𝟯𝟯 𝟴𝟭𝟴`) 
 await conn.groupLeave(id)
 try {  
 chat.welcome = true
