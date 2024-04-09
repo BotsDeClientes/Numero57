98beb1ed82437688c61926510af20bfc78e1b43a
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-04-09 14:52:56 -0500

    Update info-creadora.js

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -4,10 +4,10 @@ const handler = async (m, {conn}) => {
 handler.command = /^(Creador)$/i;
 export default handler;
 global.Creador = `
-🐧 𝗣𝗶𝗻𝗴𝘂𝗶 𝗖𝗿𝗲𝗮𝗱𝗼𝗿 🐧
+𝐁𝐨𝐭 𝐀𝐧𝐭𝐚𝐫𝐭𝐢𝐝𝐚 ❄️
 
 𝗙𝗯 : https://web.facebook.com/profile.php?id=100094780575612
-𝗜𝗻𝘀𝘁𝗮 : https://www.instagram.com/cristoferrojasph/
+𝗜𝗻𝘀𝘁𝗮 : https://www.instagram.com/cristhofer_yallico/
 𝗪𝗽𝘀 : +51 926 933 818
 
 𝗣𝗼𝗿 𝗦𝗶 𝗗𝗲𝘀𝗲𝗮𝘀 𝗖𝗼𝗺𝗽𝗿𝗮𝗿 𝗕𝗼𝘁 𝗣𝗿𝗼𝗽𝗶𝗼 , 𝗚𝗿𝘂𝗽𝗼 𝗢 𝗛𝗮𝗰𝗲𝗿 𝗖𝗼𝗹𝗮𝗯𝗼𝗿𝗮𝗰𝗶𝗼𝗻 🐧
