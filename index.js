6834c425c382eea406c1d3d1492f7532c22adb17
Xxpor12 <159817076+Xxpor12@users.noreply.github.com>
2024-05-30 10:26:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const rl = createInterface(process.stdin, process.stdout)
 //const app = express()
 //const port = process.env.PORT || 8080;
 
-say('Pingui\nDios\nSex', {
+say('Cris\nX\nJenni', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
